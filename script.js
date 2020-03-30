34274280a6de644a30d1691fdb289e8e89d19446
moygospadin <60549587+moygospadin@users.noreply.github.com>
2020-03-30 17:46:39 +0300

    feat: smooth scroll

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 window.onload = function() {
     onChangeScroll();
+    /*Скролл */
+    const anchors = document.querySelectorAll('a[href*="#"]');
+
+    for (let anchor of anchors) {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
+
+            const blockID = anchor.getAttribute('href').substr(1);
+
+            document.getElementById(blockID).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        });
+    }
     /*------------menu---------*/
     const MENU = document.getElementById('menu');
     MENU.addEventListener('click', (event) => {
